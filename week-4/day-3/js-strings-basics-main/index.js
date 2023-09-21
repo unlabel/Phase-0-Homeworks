dab879a16ee41f41c100db309279ee1c20c71e91
Denis Ignatenko <luuffyakka@yandex.ru>
2023-09-21 11:59:49 +0300

    Release 1-2

---
+++ week-4/day-3/js-strings-basics-main/index.js
@@ -0,0 +1,27 @@
+// const firstWord = 'Hello';
+// const secondWord = 'world';
+// const myConcatReversed = secondWord + firstWord;
+// console.log(myConcatReversed.split('').reverse().join(''));
+
+
+// function myConcatReversed(str1, str2) {
+//     let firstStr = str1.split('').reverse().join('');
+//     let secondStr = str2.split('').reverse().join('');
+//     return firstStr + secondStr
+// }
+// console.log(myConcatReversed("Hello", "World")); // olleHdlrow
+
+
+// String.prototype.myCamelCase = function() {
+//     let arr = this.toLowerCase().split(" ");
+//     let result = "";
+//     for (let i = 0; i < arr.length; i++) {
+//         if (i === 0) {
+//             result += arr[i].toLowerCase();
+//         } else {
+//             result += arr[i][0].toUpperCase() + arr[i].slice(1, arr[i].length);
+//         }
+//     }
+//     return result;
+// }
+// console.log('JS is so coooool!'.myCamelCase()); //jsIsSoCoooool!
