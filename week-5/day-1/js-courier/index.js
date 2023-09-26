ec1d6b88e40006a11f15aab5c2562816c43f27e6
Denis Ignatenko <luuffyakka@yandex.ru>
2023-09-26 14:59:09 +0300

    Start

---
+++ week-5/day-1/js-courier/index.js
@@ -0,0 +1,34 @@
+
+
+
+const home = [
+    151151,
+    'My home',
+    'Москва, ш. Рублевское, 1, 1'
+]
+
+const work = [
+    151151,
+    'My New home',
+    'Москва, ул. Твардовского, 2, 2'
+]
+
+const testLocation1 = [
+    0,
+    'тестовый офис',
+    'тестовая улица, 1'
+]
+const testLocation2 = [
+    1,
+    'тестовый офис 1',
+    'тестовая улица, 2'
+]
+
+const locations = []
+
+
+function add(location){
+    
+}
+
+
