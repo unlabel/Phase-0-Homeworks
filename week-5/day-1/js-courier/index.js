f078255a47e82acead21b8ee1bcb62ead8a8a8fa
Denis Ignatenko <luuffyakka@yandex.ru>
2023-09-26 15:23:41 +0300

    Release 2

--- week-5/day-1/js-courier/index.js
+++ week-5/day-1/js-courier/index.js
@@ -2,10 +2,25 @@ const home = [151151, "Home", "Москва, ш. Рублевское, 1, 1"];
 
 const work = [151151, "Elbrus", "Москва, ул. Твардовского, 2, 2"];
 
-const gym = [150050, "Ironman", "Симферополь, ул. Пушкина, 1"];
+const gym = [295000, "Ironman", "Симферополь, ул. Пушкина, 1"];
 
-const chill = [150050, "Playstation Club", "Симферополь, ул. Горького, 2"];
+const chill = [295000, "Playstation Club", "Симферополь, ул. Горького, 2"];
 
 const locations = [];
 
-function add(location) {}
+function add(location) {
+  const postalCode = location[0];
+
+  if (!locations[postalCode]) {
+    locations[postalCode] = [];
+  }
+
+  locations[postalCode].push(location.slice(1)); // Добавляем вложенный массив без почтового индекса
+}
+
+add(home);
+add(work);
+add(gym);
+add(chill);
+
+console.log(locations);
