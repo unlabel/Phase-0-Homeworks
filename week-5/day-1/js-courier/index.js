db93b3df95f059fc47ab0391f52c3a719d947855
Denis Ignatenko <luuffyakka@yandex.ru>
2023-09-26 15:10:08 +0300

    Release 1: Создание локаций

--- week-5/day-1/js-courier/index.js
+++ week-5/day-1/js-courier/index.js
@@ -1,34 +1,11 @@
+const home = [151151, "Home", "Москва, ш. Рублевское, 1, 1"];
 
+const work = [151151, "Elbrus", "Москва, ул. Твардовского, 2, 2"];
 
+const gym = [150050, "Ironman", "Симферополь, ул. Пушкина, 1"];
 
-const home = [
-    151151,
-    'My home',
-    'Москва, ш. Рублевское, 1, 1'
-]
+const chill = [150050, "Playstation Club", "Симферополь, ул. Горького, 2"];
 
-const work = [
-    151151,
-    'My New home',
-    'Москва, ул. Твардовского, 2, 2'
-]
+const locations = [];
 
-const testLocation1 = [
-    0,
-    'тестовый офис',
-    'тестовая улица, 1'
-]
-const testLocation2 = [
-    1,
-    'тестовый офис 1',
-    'тестовая улица, 2'
-]
-
-const locations = []
-
-
-function add(location){
-    
-}
-
-
+function add(location) {}
