8536f7e36437211854f89e59409f7bc8fc30f971
Denis Ignatenko <luuffyakka@yandex.ru>
2023-09-27 10:04:59 +0300

    Release 1: Цикл for

--- week-5/day-2/js-loops-houses-main/index.js
+++ week-5/day-2/js-loops-houses-main/index.js
@@ -1,19 +1,43 @@
+// const evenHouses = [];
+// const oddHouses = [];
+
+// function generateHouses(street, count, startNumber) {
+//     for (let i = 1; i <= count; i++) {
+//         const houseNumber = startNumber + i - 1;
+//         const houseName = `${street}, д.${houseNumber}`;
+
+//         if (houseNumber % 2 === 0) {
+//             evenHouses.push(houseName);
+//         } else {
+//             oddHouses.push(houseName);
+//         }
+//     }
+// }
+
+// generateHouses('ул. Тестовая', 4, 99);
+// console.log(evenHouses);
+// console.log(oddHouses);
+
 const evenHouses = [];
 const oddHouses = [];
 
 function generateHouses(street, count, startNumber) {
+  if (startNumber % 2 === 0) {
     for (let i = 1; i <= count; i++) {
-        const houseNumber = startNumber + i - 1;
-        const houseName = `${street}, д.${houseNumber}`;
-
-        if (houseNumber % 2 === 0) {
-            evenHouses.push(houseName);
-        } else {
-            oddHouses.push(houseName);
-        }
+      const evenHousesNumber = startNumber + 2 * i;
+      const evenHouseName = `${street}, ${evenHousesNumber}`;
+      evenHouses.push(evenHouseName).trim();
     }
+    return evenHouses;
+  } else {
+    for (let j = 1; j <= count; j++) {
+      const oddHousesNumber = startNumber + 2 * j - 1;
+      const oddHouseName = `${street}, ${oddHousesNumber}`;
+      oddHouses.push(oddHouseName).trim();
+    }
+    return oddHouses;
+  }
 }
 
-generateHouses('ул. Тестовая', 4, 99);
-console.log(evenHouses);
-console.log(oddHouses);
+console.log(generateHouses("Pushkina", 4, 42));
+console.log(generateHouses("Tolstogo", 3, 41));
