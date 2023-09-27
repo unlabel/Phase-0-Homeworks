07ce7a03d27b7f1c8f176396d0f9a688e533cdc7
Denis Ignatenko <luuffyakka@yandex.ru>
2023-09-27 10:46:37 +0300

    Цикл while

--- week-5/day-2/js-loops-houses-main/index.js
+++ week-5/day-2/js-loops-houses-main/index.js
@@ -18,25 +18,51 @@
 // console.log(evenHouses);
 // console.log(oddHouses);
 
+// Release 1:
+// const evenHouses = [];
+// const oddHouses = [];
+
+// function generateHouses(street, count, startNumber) {
+//   if (startNumber % 2 === 0) {
+//     for (let i = 1; i <= count; i++) {
+//       const evenHousesNumber = startNumber + 2 * (i - 1);
+//       const evenHouseName = `${street}, ${evenHousesNumber}`;
+//       evenHouses.push(evenHouseName);
+//     }
+//     return evenHouses;
+//   } else {
+//     for (let j = 1; j <= count; j++) {
+//       const oddHousesNumber = startNumber + 2 * (j - 1);
+//       const oddHouseName = `${street}, ${oddHousesNumber}`;
+//       oddHouses.push(oddHouseName);
+//     }
+//     return oddHouses;
+//   }
+// }
+
+// console.log(generateHouses("Pushkina", 4, 42));
+// console.log(generateHouses("Tolstogo", 3, 41));
+
+// Release 2:
+
 const evenHouses = [];
 const oddHouses = [];
 
 function generateHouses(street, count, startNumber) {
-  if (startNumber % 2 === 0) {
-    for (let i = 1; i <= count; i++) {
-      const evenHousesNumber = startNumber + 2 * i;
+  let i = 1;
+  while (i <= count) {
+    if (startNumber % 2 === 0) {
+      const evenHousesNumber = startNumber + 2 * (i - 1);
       const evenHouseName = `${street}, ${evenHousesNumber}`;
-      evenHouses.push(evenHouseName).trim();
-    }
-    return evenHouses;
-  } else {
-    for (let j = 1; j <= count; j++) {
-      const oddHousesNumber = startNumber + 2 * j - 1;
+      evenHouses.push(evenHouseName);
+    } else {
+      const oddHousesNumber = startNumber + 2 * (i - 1);
       const oddHouseName = `${street}, ${oddHousesNumber}`;
-      oddHouses.push(oddHouseName).trim();
+      oddHouses.push(oddHouseName);
     }
-    return oddHouses;
+    i++;
   }
+  return startNumber % 2 === 0 ? evenHouses : oddHouses;
 }
 
 console.log(generateHouses("Pushkina", 4, 42));
