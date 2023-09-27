7f6032ed63ebb2fe4280e9c444bf9edf8ed7f66a
Denis Ignatenko <luuffyakka@yandex.ru>
2023-09-27 15:38:11 +0300

    Update

--- week-5/day-2/js-loops-chessboard-main/index.js
+++ week-5/day-2/js-loops-chessboard-main/index.js
@@ -1,11 +1,37 @@
 // Release 1
+// function generateChessboard(size) {
+//   let board = "";
+//   if (!Number.isInteger(size) || size % 2 === 1) {
+//     throw new Error("Размер доски должен быть целым чётным числом");
+//   }
+//   for (let i = 1; i <= size; i++) {
+//     if (i % 2 === 0) {
+//       board += "░";
+//     } else {
+//       board += "▓";
+//     }
+//   }
+//   return board;
+// }
+// console.log(generateChessboard(4));
+
 function generateChessboard(size) {
+  let board = "";
   if (!Number.isInteger(size) || size % 2 === 1) {
     throw new Error("Размер доски должен быть целым чётным числом");
   }
+
+  for (let i = 0; i < size; i++) {
+    for (let j = 0; j < size; j++) {
+      if ((i + j) % 2 === 0) {
+        board += "⬜";
+      } else {
+        board += "⬛";
+      }
+    }
+    board += "\n";
+  }
+  return board;
 }
-try {
-  console.log(generateChessboard(3));
-} catch (error) {
-  console.error(error.message);
-}
+
+console.log(generateChessboard(4));
