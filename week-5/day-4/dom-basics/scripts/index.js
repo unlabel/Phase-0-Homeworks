2e4b704bf284a70af98c47feddcfdeea1466e22a
Denis Ignatenko <luuffyakka@yandex.ru>
2023-09-29 14:58:07 +0300

    Release 1

--- week-5/day-4/dom-basics/scripts/index.js
+++ week-5/day-4/dom-basics/scripts/index.js
@@ -1,1 +1,10 @@
-// Удали этот комментарий и пиши здесь код для релизов :)
+const toogles = document.getElementsByClassName("js-toggle");
+
+for (let i = 0; i < toogles.length; i += 1) {
+  toogles[i].addEventListener("click", (event) => {
+    const checkbox = event.target;
+    if (checkbox.checked) {
+      confetti();
+    }
+  });
+}
